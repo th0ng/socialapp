97c160b0c2a9350671ec7e93c321739ad0b698a1
Thongzxje <thonghoangdanh@gmail.com>
2022-11-25 11:06:47 +0200

    adding axios for frontend

--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -1,1 +1,31 @@
-const baseUrl = "/api/posts";
+import axios from "axios";
+const baseUrl = '/api/notes';
+
+let token = null;
+
+const setToken = (newToken) => {
+    token = `bearer ${newToken}`;
+};
+
+const getAll = () => {
+    const request = axios.get(baseUrl);
+    return request.then(response => response.data);
+}
+
+const create = async newObject => {
+    const config = {
+        headers: {Authorization: token},
+    }
+    const request = axios.post(baseUrl, newObject, config);
+    return request.then(response => response.data);
+}
+
+const remove = id => {
+    const elementUrl = `/api/notes/${id}`;
+    return axios.delete(elementUrl)
+        .catch(error => {console.error('There was an error!', error);
+    });
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {getAll, create, remove, setToken}
